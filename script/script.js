75f12e37c7130ea239053a2853524571303c1bfe
Daniel Bowen <djbowen95@gmail.com>
2023-08-17 00:50:47 +0100

    Reorder and tidy code

--- script/script.js
+++ script/script.js
@@ -1,4 +1,5 @@
 const APIKey = "96e6602198cf73c73eb7efc693fa0bdb";
+const citySearch = $('form');
 
 // Fetch the longitude / latitude when given a city name.
 function getLongLat(city) {
@@ -46,18 +47,6 @@ function processCurrentWeather(data) {
   displayCurrent(today);
 }
 
-// Uses jQuery to insert today's weather data into html elements.
-function displayCurrent(today) {
-  
-  $('.today-display img').attr('src', `../icons/${today.icon}.png`);
-  $('.today-display .temp .marker').text(`${convertTemp(today.temp)}`);
-  $('.today-display .humidity .marker').text(`${today.humidity}%`);
-  $('.today-display .wind-speed .marker').text(`${convertWindSpeed(today.wind_speed)}`);
-  $('.today-tisplay .uv-index').text(`${today.uvi}`);
-  
-  return;
-}
-
 // Iterates through weather for next five days, logs to the console.
 function processForecast (data) {
   for (let i = 0; i < 5; i++) {
@@ -75,16 +64,40 @@ function processForecast (data) {
   return;
 }
 
+// Uses jQuery to insert today's weather data into html elements.
+function displayCurrent(today) {
+  
+  $('.today-display img').attr('src', `../icons/${today.icon}.png`);
+  $('.today-display .temp .marker').text(`${convertTemp(today.temp)}`);
+  $('.today-display .humidity .marker').text(`${today.humidity}%`);
+  $('.today-display .wind-speed .marker').text(`${convertWindSpeed(today.wind_speed)}`);
+  $('.today-tisplay .uv-index').text(`${today.uvi}`);
+  
+  return;
+}
+
 function displayForecast(index, current) {
 
-$(`#future-block-${index} img`).attr('src', `../icons/${current.icon}.png`);
-$(`#future-block-${index} .temp-marker`).text(`${convertTemp(current.temp)}`);
-$(`#future-block-${index} .humidity-marker`).text(`${current.humidity}%`);
-$(`#future-block-${index} .windspeed-marker`).text(`${convertWindSpeed(current.wind_speed)}`);
+  $(`#future-block-${index} img`).attr('src', `../icons/${current.icon}.png`);
+  $(`#future-block-${index} .temp-marker`).text(`${convertTemp(current.temp)}`);
+  $(`#future-block-${index} .humidity-marker`).text(`${current.humidity}%`);
+  $(`#future-block-${index} .windspeed-marker`).text(`${convertWindSpeed(current.wind_speed)}`);
 
 return;
 }
 
+// Function for handling form input
+function handleSearch (event) {
+  event.preventDefault();
+  
+  const city = $('input#search-box-input').val();
+  $('.today-display h2 ins').text(city);
+  getLongLat(city);
+
+  $('input#search-box-input').val('');
+}
+
+// Helper functions
 function convertTemp (temp) {
   const celsius = Math.round(temp - 273.15);
   return `${celsius}°C`
@@ -95,4 +108,6 @@ function convertWindSpeed(wind_speed) {
   return `${mph}mph`;
 }
 
+// Uses Birmingham as a default.
 getLongLat("Birmingham");
+citySearch.on("submit", handleSearch);
